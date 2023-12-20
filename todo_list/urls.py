5cf72d8b25bb05788c350c817adce4017f19eb1f
krishbale <sushil.pokharel.7528@gmail.com>
2023-12-20 19:54:52 +0545

    vercel deploy

---
+++ todo_list/urls.py
@@ -0,0 +1,11 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+   path('', views.todo_list, name='todo_list'),
+    path('add', views.add_task, name='add_task'),
+    path('task/<int:pk>', views.task_detail, name='task_detail'),
+    path('edit/<int:pk>', views.edit_task, name='edit_task'),
+    path('delete/<int:pk>', views.delete_task, name='delete_task'),
+    
+] 
